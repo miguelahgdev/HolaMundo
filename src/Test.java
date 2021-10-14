9e4e59366163a48ba152c36598e86f2a6411255b
miguelahg <miguelahg@ext.inditex.com>
2021-10-14 12:53:39 +0200

    Agregando la clase Test

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+
+    private String nombreTest;
+
+    public String getNombreTest() {
+        return nombreTest;
+    }
+
+    public void setNombreTest(String nombreTest) {
+        this.nombreTest = nombreTest;
+    }
+}
